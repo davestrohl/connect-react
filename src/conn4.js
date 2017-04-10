344ac07413324872d14da2e3953c1d03f4012f62
davestrohl <dave.strohl@tivix.com>
2017-04-10 16:29:52 -0400

    Working board solver

--- src/conn4.js
+++ src/conn4.js
@@ -92,13 +92,107 @@ class Conn4 extends React.Component {
         if (this.state.moveNumber < this.winCondition * 2 - 1)
             return null;
 
+        // Will be set on empty cell. Doesn't apply to diagonals
+        let ignoreColumn = new Array(this.boardColumns).fill(false);
 
         // Ignore vertical portions of crawl
         let verticalBypasses = nextCells.map((nextCell, i) => {
             return this.boardRows - nextCell <= this.winCondition;
         });
 
-        return boardState[0][0] !== 'E' ? this.state.currentPlayer : null;
+        // The last cells to check
+        let verticalBound = this.winCondition - 1;
+        let horizontalBound = this.boardColumns - this.winCondition;
+        let backwardDiagonalBound = this.winCondition - 1;
+
+
+        let crawlCells = function(row, column, color, direction, recurseCounter) {
+            // return {
+            //         win: false,
+            //         row: row,
+            //         column: column
+            //     };
+            let currentColor = boardState[column][row];
+            if (currentColor === 'E') {
+                if (direction.columnDelta === 1 && direction.rowDelta === 0)
+                    ignoreColumn[column] = true;
+                verticalBypasses[column] = true;
+                return {
+                    win: false,
+                    row: row,
+                    column: column,
+                    color: currentColor
+
+                };
+            }
+
+            if (!color) {
+                return crawlCells(row + direction.rowDelta, column + direction.columnDelta, currentColor, direction, recurseCounter - 1);
+            }
+
+            if (color === currentColor) {
+                if (recurseCounter === 0) {
+                    return {
+                        win: true,
+                        row: row,
+                        column: column,
+                        color: currentColor
+                    }
+                }
+
+                return crawlCells(row + direction.rowDelta, column + direction.columnDelta, currentColor, direction, recurseCounter - 1);
+            }
+            else {
+                return {
+                    win: false,
+                    row: row,
+                    column: column,
+                    color: currentColor
+                }
+            }
+        };
+
+        for(let currentRow=this.boardRows - 1; currentRow >= 0; currentRow--) {
+
+            let nextHorizontal = 0;
+            for (let currentColumn = 0; currentColumn < this.boardColumns; currentColumn++) {
+                if (!ignoreColumn[currentColumn]){
+                    if (!verticalBypasses[currentColumn] && currentRow > verticalBound) {
+                        let verticalResult = crawlCells(currentRow, currentColumn, null, {rowDelta: -1, columnDelta: 0}, this.winCondition - 1);
+                        if (verticalResult.win) {
+                            return verticalResult.color;
+                        }
+
+                    }
+                    if (currentColumn <= horizontalBound && currentColumn >= nextHorizontal) {
+                        let horizontalResult = crawlCells(currentRow, currentColumn, null, {rowDelta: 0, columnDelta: 1}, this.winCondition - 1);
+                        if (horizontalResult.win) {
+                            return horizontalResult.color;
+                        }
+                        nextHorizontal = horizontalResult.column;
+                    }
+
+                    if (currentRow > verticalBound) {
+                        if (currentColumn <= horizontalBound) {
+                            let forwardDiagonalResult = crawlCells(currentRow, currentColumn, null, {rowDelta: -1, columnDelta: 1}, this.winCondition - 1);
+                            if (forwardDiagonalResult.win) {
+                                return forwardDiagonalResult.color;
+                            }
+                        }
+
+                        if (currentColumn >= backwardDiagonalBound) {
+                            let backwardDiagonalResult = crawlCells(currentRow, currentColumn, null, {rowDelta: -1, columnDelta: -1}, this.winCondition - 1);
+                            if (backwardDiagonalResult.win) {
+                                return backwardDiagonalResult.color;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return null;
     }
 
     dropPiece(column) {
